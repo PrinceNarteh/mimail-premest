6a1bd1512cb8f2093e0aaf0c425f69bcf7ce3661
PrinceNarteh <prinarteh@gmail.com>
2022-01-27 10:55:00 +0000

    refactor the models

--- backend/src/models/mail.model.js
+++ backend/src/models/mail.model.js
@@ -1,31 +1,32 @@
 const mongoose = require("mongoose");
 
-const mailSchema = new mongoose.Schema({
-  createdAt: {
-    type: Date,
-    default: Date.now
+const mailSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: String,
+      required: true,
+    },
+    recipient: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: [true, "enter a message title"],
+    },
+    body: {
+      type: String,
+      required: [true, "enter a message body"],
+    },
   },
-  sender: {
-    type: String,
-    required: true
-  },
-  recipient: {
-    type: String,
-    required: true
-  },
-  read: {
-    type: Boolean,
-    default: false
-  },
-  title: {
-    type: String,
-    required: [true, "enter a message title"]
-  },
-  body: {
-    type: String,
-    required: [true, "enter a message body"]
+  {
+    timestamps: true,
   }
-});
+);
 
-const Mail = mongoose.model("mail", mailSchema);
+const Mail = mongoose.model("Mail", mailSchema);
 module.exports = Mail;
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,31 +1,37 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "please enter a username"],
-    minlength: [6, "minimum username length is 6 characters"],
-    unique: true,
-    lowercase: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "please enter a username"],
+      minlength: [6, "minimum username length is 6 characters"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "you need to choose a password"],
+      minlength: [6, "minimum password length is 6 characters"],
+    },
+    sent: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "mail",
+      },
+    ],
+    inbox: [{ type: mongoose.Schema.Types.ObjectId, ref: "mail" }],
   },
-  password: {
-    type: String,
-    required: [true, "you need to choose a password"],
-    minlength: [6, "minimum password length is 6 characters"]
-  },
-  sent: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "mail"
-    }
-  ],
-  inbox: [{ type: mongoose.Schema.Types.ObjectId, ref: "mail" }]
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
-  const salt = 10;
-  this.password = await bcrypt.hash(this.password, salt);
+  if (this.isModified(this.password)) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
   next();
 });
 
@@ -39,10 +45,10 @@ userSchema.statics.login = async function (username, password) {
       return user;
     }
 
-    throw new Error("incorrect password");
+    throw new Error("Invalid credentials");
   }
 
-  throw new Error("incorrect username");
+  throw new Error("Invalid credentials");
 };
 
 const User = mongoose.model("user", userSchema);
