3d256e7d33de02709604dbb20913c05d877a482f
PrinceNarteh <prinarteh@gmail.com>
2022-02-04 03:49:46 +0000

    implements mail reducer

--- frontend/src/context/mail/mail.reducer.tsx
+++ frontend/src/context/mail/mail.reducer.tsx
@@ -16,14 +16,31 @@ const replaceMail = (arr: MailType[], action: ActionType) => {
   });
 };
 
-export const mailReducer = (state: StateType, action: ActionType) => {
-  switch (action.type) {
-    case MailActionTypes.TOGGLE_STARRED: {
-      let {
-        mails: { inbox, sent },
-      } = state;
-      const routeName = action.payload.routeName.substring(1);
+const deleteMail = (arr: MailType[], action: ActionType) => {
+  return arr.filter((mail: MailType) => mail._id !== action.payload.mail);
+};
 
+export const mailReducer = (
+  state: StateType,
+  action: ActionType
+): StateType => {
+  let {
+    mails: { inbox, sent },
+  } = state;
+  const routeName = action.payload.routeName.substring(1);
+
+  switch (action.type) {
+    case MailActionTypes.ADD_MAILS: {
+      return {
+        ...state,
+        mails: { inbox: action.payload.inbox, sent: action.payload.sent },
+      };
+    }
+    case MailActionTypes.ADD_MAIL: {
+      const newInbox = [action.payload.mail, ...state.mails.inbox];
+      return { ...state, mails: { inbox: newInbox, sent } };
+    }
+    case MailActionTypes.TOGGLE_STARRED: {
       if (routeName === "inbox") {
         inbox = replaceMail(inbox, action);
       } else if (routeName === "sent") {
@@ -34,6 +51,21 @@ export const mailReducer = (state: StateType, action: ActionType) => {
           inbox = replaceMail(inbox, action);
         } else {
           sent = replaceMail(sent, action);
+        }
+      }
+      return { ...state, mails: { inbox, sent } };
+    }
+    case MailActionTypes.DELETE_MAIL: {
+      if (routeName === "inbox") {
+        inbox = deleteMail(inbox, action);
+      } else if (routeName === "sent") {
+        sent = deleteMail(sent, action);
+      } else if (routeName === "starred") {
+        const mail = inbox.find((mail) => mail._id === action.payload.mail._id);
+        if (mail) {
+          inbox = deleteMail(inbox, action);
+        } else {
+          sent = deleteMail(sent, action);
         }
       }
       return { ...state, mails: { inbox, sent } };
